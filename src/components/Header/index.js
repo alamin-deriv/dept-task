aaaa3763cec70efccb3365b0f73d6accf79d239d
Raphael Nweke <raphael.nweke@flexisaf.net>
2022-05-26 14:50:54 +0100

    menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,12 @@
+import React, {useState} from 'react'
 import styled from 'styled-components';
 import LogoBlack from '../../assets/logoBlack.png';
+import LogoWhite from '../../assets/logoWhite.svg';
 import MenuBar from '../../assets/menuBar.svg';
 
 import device from './../../utils/device'
+
+import {SideSheet, SheetContainer} from '../Menu'
 
 
 
@@ -45,13 +49,52 @@ const HeaderContainer = styled.div`
 `
 
 export const Header = () => {
+    const [basic, setBasic] = useState(false)
     return (
+        <>
+        <SideSheet width="100vw" open={basic} onClose={() => setBasic(false)}>
+            <SheetContainer>
+                <div>
+                    <img src={LogoWhite} alt="Logo" className="logo" height="38px"/>
+                </div>
+                <ul>
+                    <li>
+                        <span>Home</span>
+                    </li>
+                    <li>
+                        <span>Work</span>
+                    </li>
+                    <li>
+                        <span>Culture</span>
+                    </li>
+                    <li>
+                        <span>Services</span>
+                    </li>
+                    <li>
+                        <span>Partners</span>
+                    </li>
+                    <li>
+                        <span>Stories</span>
+                    </li>
+                    <li>
+                        <span>Careers</span>
+                    </li>
+                    <li>
+                        <span>Events</span>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </SheetContainer>
+        </SideSheet>
         <HeaderContainer>
            <img src={LogoBlack} alt="Logo" className="logo" />
-           <div className="menu">
-           <h2 >MENU</h2>
-           <img src={MenuBar} alt="menu"  />
+           <div className="menu" onClick={() => setBasic(!basic)}>
+            <h2 >MENU</h2>
+            <img src={MenuBar} alt="menu"  />
            </div>
         </HeaderContainer>
+        </>
     )
 }
